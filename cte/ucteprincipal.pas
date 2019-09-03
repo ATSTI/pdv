cf098aa2f20c871c81e95bd5f123dab6a00fcdab
ATSTI <ats@atsti.com.br>
2019-09-03 13:16:05 -0300

    Projeto CTe

---
+++ cte/ucteprincipal.pas
@@ -0,0 +1,26 @@
+unit uCtePrincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfCTePrincipal = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  fCTePrincipal: TfCTePrincipal;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
