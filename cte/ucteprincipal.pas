97af8ab5a4582f064ac81eba5fa7cbce5576db33
NilCarvalhoATS <claudecircarvalho@gmail.com>
2019-09-03 14:56:04 -0300

    Iniciando criaço da tela

--- cte/ucteprincipal.pas
+++ cte/ucteprincipal.pas
@@ -5,10 +5,22 @@ unit uCtePrincipal;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls;
 
 type
+
+  { TfCTePrincipal }
+
   TfCTePrincipal = class(TForm)
+    PageControl1: TPageControl;
+    PageControl2: TPageControl;
+    PageControl3: TPageControl;
+    Panel1: TPanel;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    TabSheet4: TTabSheet;
+    TabSheet5: TTabSheet;
   private
 
   public
