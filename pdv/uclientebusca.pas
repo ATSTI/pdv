2b25446b5811797f9f085081a879e8057d4233e4
ATSTI <ats@atsti.com.br>
2019-09-20 15:33:25 -0300

    adicionado o contas a receber

--- pdv/uclientebusca.pas
+++ pdv/uclientebusca.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  StdCtrls, DBGrids, Buttons, db, udmpdv;
+  StdCtrls, DBGrids, Buttons, db, udmpdv, uPadraoBusca;
 
 type
 
