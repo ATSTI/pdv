5082d9524dcc5aa97e7f954fdbed826f5bb08267
manoel-ats <manmdsantos@yahoo.com.br>
2019-10-04 10:18:23 -0300

    Projeto NFe - Iniciando

--- nfe/unfe.pas
+++ nfe/unfe.pas
@@ -5,10 +5,16 @@ unit uNFe;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls;
 
 type
+
+  { TfNFe }
+
   TfNFe = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
+    StatusBar1: TStatusBar;
   private
 
   public
