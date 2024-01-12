e4a18316b938a483da8e3ee60eebb54c6b7a843f
Carlos Silveira <crsilveira@gmail.com>
2024-01-12 18:05:48 -0300

    chamar a executacao por estes arquivos

---
+++ nfe/nfe_odoo/executa_odoo_nfe_busca.py
@@ -0,0 +1,14 @@
+from conecta_odoo_nfe import ConectaServerNFe as nfe_envia
+
+class ExecutaOdooNFeBusca():
+    _name = 'executa.odoo.nfe.busca'
+
+    def __init__(self):
+        #import pudb;pu.db
+        empresa = int('999999') # ao executar o script substitui pelo id da empresa
+        # uso TESTE
+        # empresa = '3'
+        conecta = nfe_envia(empresa=empresa, funcao_exec='PEGA_XML')
+        conecta.pega_xml(int(empresa))
+
+ExecutaOdooNFeBusca()
---
+++ nfe/nfe_odoo/executa_odoo_nfe_return.py
@@ -0,0 +1,14 @@
+from conecta_odoo_nfe import ConectaServerNFe as nfe_envia
+
+class ExecutaOdooNFeRetorna():
+    _name = 'executa.odoo.nfe.retorna'
+
+    def __init__(self):
+        #import pudb;pu.db
+        empresa = int('999999') # ao executar o script substitui pelo id da empresa
+        # uso TESTE
+        # empresa = '3'
+        conecta = nfe_envia(empresa=empresa, funcao_exec='ENVIA_AUTORIZADA')
+        conecta.retorna_xml_validado(int(empresa))
+
+ExecutaOdooNFeRetorna()
